d4248dee0ba2e4da62c39952afc15cfa5e101e32
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-11 21:59:40 +0600

    solved task1

--- task1.js
+++ task1.js
@@ -1,10 +1,18 @@
+// Task-1
+
 // Broken Conditional
 
 function checkAdult(age) {
-  if ((age = 18)) {
+  if (age >= 18) {
     return "Adult";
   }
   return "Minor";
 }
 
-console.log(checkAdult(15));
+// console.log(checkAdult(18));
+
+// Brainstorming:
+// 1) Did I compare or assign the value?
+// 2) Why is every result becoming "Adult"?
+
+
