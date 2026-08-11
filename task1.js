c5f846998f664ed47fbc144bb863ff57553c7ec3
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-11 22:06:06 +0600

    solved task2

--- task1.js
+++ task1.js
@@ -15,4 +15,21 @@ function checkAdult(age) {
 // 1) Did I compare or assign the value?
 // 2) Why is every result becoming "Adult"?
 
+// -----------------------------------------------------------------
 
+// Task-2
+
+// Broken Conditional
+
+function canApply(age, hasNID) {
+  if (age >= 18 && hasNID) {
+    return "Eligible";
+  }
+  return "Not Eligible";
+}
+
+// console.log(canApply(35, true));
+
+// Brainstorming:
+// 1) Should BOTH conditions be true to apply?
+// 2) Are we using the correct logical operator?
