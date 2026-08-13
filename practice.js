298b041763a95a5e0c23340945f4819c46f332bc
nuruddin2971 <nuruddin2971@gmail.com>
2026-08-13 23:49:30 +0600

    solved task-1

---
+++ practice.js
@@ -0,0 +1,18 @@
+// Task 14-1: Spot the Error Type
+
+// // Bug type: SyntaxError
+// function sayHi(name) {
+//   return "Hi, " + name;
+// }
+// const greeting = sayHi("Mira");
+// console.log(greeting);
+
+// // Bug type: TypeError
+// let word = "hello";
+// console.log(word.toUpperCase());
+
+// // Bug type: Logical bug
+// function square(num) {
+//   return num * num;
+// }
+// console.log(square(4));
